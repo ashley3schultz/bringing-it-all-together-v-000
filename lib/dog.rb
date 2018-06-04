b092f6ff6289a383249752dea7f2fea854cfd85d
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-04 01:32:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@ class Dog
   attr_accessor :name, :breed, :id
 
   def initialize(name: name, breed: breed, id: id = nil)
-    @name = name 
-    @breed = breed 
+    @name = name
+    @breed = breed
     @id = id
   end
 end
