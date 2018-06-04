450cfbf6e78ce80fe6907fc50a510af22a1da76f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-04 02:11:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,7 @@ class Dog
 
   def self.find_or_create_by(dog)
     row = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?",dog[:name], dog[:breed]).flatten
-    find_by_id(row[0]) || create(dog)
+    find_by_id(row[0]) || self.new(name: dog[:name], breed: dog[:breed]).save
   end
 
   def self.find_by_name(name)
